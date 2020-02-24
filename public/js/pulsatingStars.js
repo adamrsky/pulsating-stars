5abe63374d94d23bb69fcfff6c385a3cb0817f97
madixo <minemadix@gmail.com>
2020-02-24 19:01:08 +0100

    Code improvements

--- public/js/pulsatingStars.js
+++ public/js/pulsatingStars.js
@@ -11,7 +11,7 @@ class PulsatingStars {
 
     this.canvas = canvas;
     this.starSize = options.starSize || 26;
-    this.starGap = options.starGap || 22;
+    this.starSpacing = options.starSpacing || 22;
 
     this.init();
 
@@ -26,14 +26,14 @@ class PulsatingStars {
 
     this.stars = [];
 
-    this.tileRowCount = Math.floor(width / this.starSize);
-    this.tileColumnCount = Math.floor(height / this.starSize);
+    this.tileRowCount = Math.floor((width) / (this.starSize + this.starSpacing));
+    this.tileColumnCount = Math.floor((height) / (this.starSize + this.starSpacing));
     this.amountOfStars = this.tileRowCount * this.tileColumnCount;
 
     for (let i = 0; i < this.amountOfStars; i++) {
       let x = i % this.tileRowCount;
       let y = Math.floor(i / this.tileRowCount);
-      this.addStar(new Star(x, y, this.starSize, this.starGap, this.canvas, i));
+      this.addStar(new Star(x, y, this.starSize, this.starSpacing, this.canvas, i));
     }
   }
 
@@ -48,23 +48,57 @@ class PulsatingStars {
   /**
    * Add new star.
    * @param {Star} newElement Star to be added.
-   * @return {Number} New length of stars array.
+   * @return {number} New length of stars array.
    */
   addStar(newElement) {
-
     this.stars.push(newElement);
     return this.stars.length;
+  }
 
+  /**
+   * Set stars size.
+   * @param {number} size New Star size.
+   */
+  setStarSize(size) {
+    this.starSize = size;
+  }
+
+  /**
+   * Update stars size.
+   */
+  updateStarSize() {
+    this.stars.forEach(e => {
+      e.setSize(this.starSize);
+    });
+    this.init();
+  }
+
+  /**
+   * Set stars spacing.
+   * @param {number} spacing New Star spacing.
+   */
+  setStarSpacing(spacing) {
+    this.starSpacing = spacing;
+  }
+
+  /**
+   * Update stars spacing.
+   */
+  updateStarSpacing() {
+    this.stars.forEach(e => {
+      e.setSize(this.starSpacing);
+    });
+    this.init();
   }
 
   /**
    * Draw stars.
    */
   draw() {
-    const marginX = (this.canvas.element.width - ((this.starSize + this.starGap)
-      * this.tileRowCount - this.starGap)) / 2;
-    const marginY = (this.canvas.element.height - ((this.starSize + this.starGap)
-      * this.tileColumnCount - this.starGap)) / 2;
+    const marginX = (this.canvas.element.width - ((this.starSize + this.starSpacing)
+      * this.tileRowCount - this.starSpacing)) / 2;
+    const marginY = (this.canvas.element.height - ((this.starSize + this.starSpacing)
+      * this.tileColumnCount - this.starSpacing)) / 2;
 
     this.canvas.ctx.save();
     this.canvas.ctx.translate(marginX, marginY);
